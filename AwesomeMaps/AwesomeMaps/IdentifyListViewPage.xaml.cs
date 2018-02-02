312dcc6eb0924f3ee4d056b0c91b9bde6cf7dc54
helloworld1973 <yxzhjason@gmail.com>
2018-02-02 23:02:35 +0800

    ListViewPage

---
+++ AwesomeMaps/AwesomeMaps/IdentifyListViewPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AwesomeMaps
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class IdentifyListViewPage : ContentPage
+	{
+		public IdentifyListViewPage ()
+		{
+			InitializeComponent ();
+            SpeciesListView.ItemsSource = new List<Species>
+            {
+                new Species
+                {
+                    Name="Must",
+                    Similarity=0.43,
+                    InvasiveOrNot=true,
+                    Solutions="please remove it!",
+                    ImageAddr="login_image.png"
+
+                },
+                new Species
+                {
+                    Name="Must",
+                    Similarity=0.43,
+                    InvasiveOrNot=true,
+                    Solutions="please remove it!",
+                    ImageAddr="login_image.png"
+
+                },
+                new Species
+                {
+                    Name="Must",
+                    Similarity=0.43,
+                    InvasiveOrNot=true,
+                    Solutions="please remove it!",
+                    ImageAddr="login_image.png"
+
+                }
+
+            };
+
+
+        }
+	}
+}
---
+++ AwesomeMaps/AwesomeMaps/Species.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwesomeMaps
+{
+    class Species
+    {
+        public string Name { get; set; }
+        public double Similarity { get; set; }
+        public Boolean InvasiveOrNot { get; set; }
+        public string Solutions { get; set; }
+        public string ImageAddr { get; set; }
+    }
+}
